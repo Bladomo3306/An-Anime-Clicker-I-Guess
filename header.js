9ab8745493d2f179a9b79e94dec9f5b43286265e
Blake Morris <107509415+Bladomo3306@users.noreply.github.com>
2024-04-24 21:42:11 -0500

    Added background music!!!! ...kinda

--- header.js
+++ header.js
@@ -1,10 +1,17 @@
 function openShop() {
     console.log("Opening the shop...");
     window.location.href = "../Shop/Shop.html";
+    setMusic();
+    
 }
 function reopenClicker() {
     window.location.href = "../Game/Clicker.html";
 }
 function openAchievements() {
     window.location.href = "../Achievements/Achievements.html";
+    setMusic();
+}
+function setMusic() {
+    var music = document.getElementById("backgroundMusic");
+    localStorage.setItem("MusicTime", music.currentTime);
 }
