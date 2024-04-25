3ec404acc1e2ef9255bb43606a44bacd6bd8afc0
MSDiehl <diehl.malachi990@gmail.com>
2024-04-24 23:06:02 -0500

    Added decent enough upgrades.

--- Shop/Shop.js
+++ Shop/Shop.js
@@ -4,9 +4,9 @@ function openUpgrades() {
             <div class="col-md-6 mt-2">
                 <div class="shop-item">
                     <div class="item-info">
-                        <h4 class="item-name">Extra Clicks</h4>
-                        <p class="item-description">Increases your dabloons per click.</p>
-                        <span class="item-price">Cost: 50 dabloons</span>
+                        <h4 class="item-name">Ramen Noodles</h4>
+                        <p class="item-description">Mmmm tasty. Increases your blossoms per click.</p>
+                        <span class="item-price">Cost: 50 Blossoms</span>
                     </div>
                     <button class="shop-button">Buy</button>
                 </div>
@@ -14,14 +14,106 @@ function openUpgrades() {
             <div class="col-md-6 mt-2">
                 <div class="shop-item">
                     <div class="item-info">
-                        <h4 class="item-name">+1 click a second</h4>
-                        <p class="item-description">Adds 1 dabloon a second.</p>
-                        <span class="item-price">Cost: 100 dabloons</span>
+                        <h4 class="item-name">Rem</h4>
+                        <p class="item-description">Wait... who's that again? Boosts morale! 1 blossom a second.</p>
+                        <span class="item-price">Cost: 100 Blossoms</span>
                     </div>
                     <button class="shop-button">Buy</button>
                 </div>
             </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Shadow Clone Jutsu</h4>
+                        <p class="item-description">Create shadow clones of yourself! 5 blossoms a second.</p>
+                        <span class="item-price">Cost: 500 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Astolfo Body Pillow</h4>
+                        <p class="item-description">Don't judge me... 10 blossoms a second.</p>
+                        <span class="item-price">Cost: 1500 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Cherry Blossom Tree</h4>
+                        <p class="item-description">Money doesn't grow on trees... maybe. 20 blossoms a second.</p>
+                        <span class="item-price">Cost: 5000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Mappa</h4>
+                        <p class="item-description">Don't worry they're not getting overworked :3. 50 blossoms a second.</p>
+                        <span class="item-price">Cost: 20000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Haki</h4>
+                        <p class="item-description">Boosts your click power! 2x blossoms per click.</p>
+                        <span class="item-price">Cost: 100000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Geass</h4>
+                        <p class="item-description">Take control of the world! 150 blossoms a second.</p>
+                        <span class="item-price">Cost: 500000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">Founding Titan</h4>
+                        <p class="item-description">Control all the titans! 300 blossoms a second.</p>
+                        <span class="item-price">Cost: 1500000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">All for One</h4>
+                        <p class="item-description">Take other people's powers. 600 blossoms a second.</p>
+                        <span class="item-price">Cost: 5000000 Blossoms</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+            <div class="col-md-6 mt-2">
+                <div class="shop-item">
+                    <div class="item-info">
+                        <h4 class="item-name">STEEEAAAAAAALLLLL!!!</h4>
+                        <p class="item-description">Get extra... lucky. 1200 blossoms a second.</p>
+                        <span class="item-price">Cost: 10000000</span>
+                    </div>
+                    <button class="shop-button">Buy</button>
+                </div>
+            </div>
+
         </div>
+        
     `;
     document.getElementById('shop-container').innerHTML = upgradesHTML;
     document.getElementById('upgrades-tab').classList.add('active');
